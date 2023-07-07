88ba540dfe5d3a919717b94e58727dbd0cd487e9
Reto Lehmann <retocode@icloud.com>
2023-07-07 07:17:39 +0200

    Update mesh resources

--- hack/lib/mesh.bash
+++ hack/lib/mesh.bash
@@ -134,7 +134,7 @@ function deploy_gateways {
 #    - to:
 #        - operation:
 #            hosts: [ "*.${subdomain}" ]
-EOF
+#EOF
 
   oc apply -n "${EVENTING_NAMESPACE}" -f "${resources_dir}"/kafka-service-entry.yaml || return $?
   for ns in serverless-tests eventing-e2e0 eventing-e2e1 eventing-e2e2 eventing-e2e3 eventing-e2e4; do
